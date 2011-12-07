ae2547225fab07a5d4bb419cc458a575e22c4b03
maerzbow <maerzbow@gmail.com>
2011-12-07 21:53:22 +0100

    refactoring: method extracting

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -15,26 +15,32 @@ class CsvToJson
     end
   end
 
+
   def process_file(csv_file_path)
     csv_file_path = File.expand_path csv_file_path
     json = csv_file_to_json(csv_file_path)
 
-    json_file_path = csv_file_path.sub(/\.csv$/,'.json')
+    json_file_path = convert_csv_to_json_file_path(csv_file_path)
 
     File.new(json_file_path, 'w').write(json)
 
-    rescue => e
-      puts csv_file_path + ": " + e.to_s
+  rescue => e
+    puts csv_file_path + ": " + e.to_s
   end
+
+  def convert_csv_to_json_file_path(csv_file_path)
+    csv_file_path.sub(/\.csv$/, '.json')
+  end
+
 
   def csv_file_to_json(csv_file_path)
     csv_table = CSV.read(csv_file_path, @csv_reader_options)
 
-    list =   []
+    list = []
     csv_table.each do |row|
       entry = {}
       csv_table.headers.each do |header|
-        entry[header] =  row[header]
+        entry[header] = row[header]
       end
       list << entry
     end
