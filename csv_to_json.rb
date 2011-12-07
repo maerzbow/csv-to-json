8a665350170a800ea7046cd06c03fc9e94b2753f
maerzbow <maerzbow@gmail.com>
2011-12-07 21:19:16 +0100

    parsing ARGV for filenames

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -2,14 +2,26 @@ require "csv"
 require "json"
 
 class CsvToJson
+  def process_files(csv_file_paths)
+    csv_file_paths.each do |path|
+      process_file path
+    end
+  end
 
   def process_file(csv_file_path)
+    csv_file_path = File.expand_path csv_file_path
     json = csv_file_to_json(csv_file_path)
-    puts json
+
+    json_file_path = csv_file_path.sub(/\.csv$/,'.json')
+
+    File.new(json_file_path, 'w').write(json)
+
+    rescue => e
+      puts csv_file_path + ": " + e.to_s
   end
 
   def csv_file_to_json(csv_file_path)
-    csv_table = CSV.read(csv_file_path, {headers: true, col_sep: ';', encoding: 'WINDOWS-1252'})
+    csv_table = CSV.read(csv_file_path, {headers: true, col_sep: ';', encoding: 'ISO-8859-1'})
 
     list =   []
     csv_table.each do |row|
@@ -21,8 +33,7 @@ class CsvToJson
     end
     JSON.pretty_generate(list)
   end
-
 end
 
 ctj = CsvToJson.new
-ctj.process_file('Flaechen.csv')
+ctj.process_files(ARGV)
